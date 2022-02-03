09fbf5d05d9370c20651b3a775ba1081302dc71d
PavelGuliev <pavel12345g@gmail.com>
2022-02-03 15:46:25 +0300

    intial commit

---
+++ geocoder.py
@@ -0,0 +1,155 @@
+import requests
+import math
+
+
+def find_businesses(ll, spn, request, locale="ru_RU"):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"  # вставить api_key
+    search_params = {
+        "apikey": api_key,
+        "text": request,
+        "lang": locale,
+        "ll": ll,
+        "spn": spn,
+        "type": "biz"
+    }
+
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {search_api_server}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первую найденную организацию.
+    organizations = json_response["features"]
+    return organizations
+
+
+def find_business(ll, spn, request, locale="ru_RU"):
+    orgs = find_businesses(ll, spn, request, locale=locale)
+    if len(orgs):
+        return orgs
+
+
+# Определяем функцию, считающую расстояние между двумя точками, заданными координатами
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+
+def geocode(address):
+    # Собираем запрос для геокодера.
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": API_KEY,
+        "geocode": address,
+        "format": "json"}
+
+    # Выполняем запрос.
+    response = requests.get(geocoder_request, params=geocoder_params)
+
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+    else:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    # Получаем первый топоним из ответа геокодера.
+    # Согласно описанию ответа он находится по следующему пути:
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
+# Получаем координаты объекта по его адресу.
+def get_coordinates(address):
+    toponym = geocode(address)
+    if not toponym:
+        return None, None
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Широта, преобразованная в плавающее число:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
+
+
+# Получаем параметры объекта для рисования карты вокруг него.
+def get_ll_span(address):
+    toponym = geocode(address)
+    if not toponym:
+        return (None, None)
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и Широта :
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+    # Собираем координаты в параметр ll
+    ll = ",".join([toponym_longitude, toponym_lattitude])
+
+    # Рамка вокруг объекта:
+    envelope = toponym["boundedBy"]["Envelope"]
+
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
+
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(float(l) - float(r)) / 2.0
+    dy = abs(float(t) - float(b)) / 2.0
+
+    # Собираем размеры в параметр span
+    span = f"{dx},{dy}"
+
+    return ll, span
+
+
+# Находим ближайшие к заданной точке объекты заданного типа.
+def get_nearest_object(point, kind):
+    ll = "{0},{1}".format(point[0], point[1])
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": API_KEY,
+        "geocode": ll,
+        "format": "json"}
+    if kind:
+        geocoder_params['kind'] = kind
+    # Выполняем запрос к геокодеру, анализируем ответ.
+    response = requests.get(geocoder_request, params=geocoder_params)
+    if not response:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code,} ({response.reason})""")
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первый топоним из ответа геокодера.
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"]["name"] if features else None

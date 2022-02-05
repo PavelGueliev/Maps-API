3d1930c9225bbb1e417e3de8b3f59c71f499eac3
PavelGuliev <pavel12345g@gmail.com>
2022-02-05 23:44:24 +0300

    Реализованы метки

--- mapapi_QT.py
+++ mapapi_QT.py
@@ -2,8 +2,12 @@ import os
 import sys
 import requests
 from PyQt5.QtCore import Qt
+from geocoder import get_coordinates
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from main_qt import Ui_MainWindow
+from PIL import Image, ImageQt
+from io import BytesIO
+from PyQt5.QtGui import QPixmap
 
 SCREEN_SIZE = [600, 450]
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
@@ -15,35 +19,61 @@ class Example(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.ll = [float(input()), float(input())]
         self.spn = float(input())
-        self.l = self.buttonGroup.checkedButton().text()
-        self.radioButton.clicked.connect(
-                    lambda: self.getImage(self.ll[0], self.ll[1], self.spn, self.buttonGroup.checkedButton().text())
+        self.l = self.comboBox.currentText()
+        self.comboBox.currentTextChanged.connect(
+                    lambda: self.getImage(self.ll[0], self.ll[1], self.spn, self.comboBox.currentText())
                 )
-        self.radioButton_2.clicked.connect(
-                    lambda: self.getImage(self.ll[0], self.ll[1], self.spn, self.buttonGroup.checkedButton().text())
-                )
-        self.radioButton_3.clicked.connect(
-                    lambda: self.getImage(self.ll[0], self.ll[1], self.spn, self.buttonGroup.checkedButton().text())
-                )
-
+        self.pushButton.clicked.connect(self.add_point)
+        self.pushButton_2.clicked.connect(self.del_point)
+        self.pt = None
         self.getImage(self.ll[0], self.ll[1], self.spn, self.l)
         self.setWindowTitle('Отображение карты')
 
     def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            self.spn += 0.1
         if event.key() == Qt.Key_PageUp:
-            self.spn += 1
-        elif event.key() == Qt.Key_PageDown:
-            if self.spn - 1 > 0:
-                self.spn -= 0.01
-        elif event.key() == Qt.Key_A:
-            self.ll[0] -= 0.01
-        elif event.key() == Qt.Key_D:
-            self.ll[0] += 0.01
-        elif event.key() == Qt.Key_W:
-            self.ll[1] += 0.01
-        elif event.key() == Qt.Key_S:
-            self.ll[1] -= 0.01
-        self.getImage(self.ll[0], self.ll[1], self.spn, self.buttonGroup.checkedButton().text())
+            if self.spn - 0.1 > 0:
+                self.spn -= 0.1
+        if event.key() == Qt.Key_A or event.key() == Qt.Key_Left:
+            if self.ll[0] - self.spn * 4 > -180:
+                self.ll[0] -= self.spn * 4
+            else:
+                self.ll[0] -= self.spn * 4
+                self.ll[0] %= 180
+        if event.key() == Qt.Key_D or event.key() == Qt.Key_Right:
+            if self.ll[0] + self.spn * 4 < 180:
+                self.ll[0] += self.spn * 4
+            else:
+                self.ll[0] += self.spn * 4
+                self.ll[0] %= 180
+        if event.key() == Qt.Key_W or event.key() == Qt.Key_Up:
+            if self.ll[1] + self.spn * 1.5 < 80.0:
+                self.ll[1] += self.spn * 1.5
+            else:
+                self.ll[1] += self.spn * 1.5
+                self.ll[1] %= 80
+        if event.key() == Qt.Key_S or event.key() == Qt.Key_Down:
+            if self.ll[1] - self.spn * 1.5 > -80:
+                self.ll[1] -= self.spn * 1.5
+            else:
+                self.ll[1] -= self.spn * 1.5
+                self.ll[1] %= 80
+        self.getImage(self.ll[0], self.ll[1], self.spn, self.comboBox.currentText())
+
+    def add_point(self):
+        try:
+            self.pt = get_coordinates(self.lineEdit.text())
+            self.ll = self.pt[::]
+            self.getImage(self.ll[0], self.ll[1], self.spn, self.comboBox.currentText())
+        except Exception:
+            self.pt = None
+            self.lineEdit.clear()
+
+    def del_point(self):
+        self.pt = None
+        self.lineEdit.clear()
+        self.getImage(self.ll[0], self.ll[1], self.spn, self.comboBox.currentText())
 
     def getImage(self, ll_1, ll_2, spn, l):
         try:
@@ -57,12 +87,21 @@ class Example(QMainWindow, Ui_MainWindow):
         elif l == 'гибрид':
             l = 'sat,skl'
         map_request = "http://static-maps.yandex.ru/1.x/"
-        map_params = {
-            "apikey": API_KEY,
-            "ll": f'{ll_1},{ll_2}',
-            "spn": f'{spn},{spn}',
-            "l": l
-        }
+        if not self.pt:
+            map_params = {
+                "apikey": API_KEY,
+                "ll": f'{ll_1},{ll_2}',
+                "spn": f'{spn},{spn}',
+                "l": l
+            }
+        else:
+            map_params = {
+                "apikey": API_KEY,
+                "ll": f'{ll_1},{ll_2}',
+                "spn": f'{spn},{spn}',
+                "l": l,
+                'pt': f'{self.pt[0]},{self.pt[1]},flag'
+            }
         response = requests.get(map_request, params=map_params)
 
         if not response:
@@ -76,6 +115,9 @@ class Example(QMainWindow, Ui_MainWindow):
             file.write(response.content)
         print(self.spn)
         self.label.setStyleSheet(f"border-image:url(map.png)")
+
+        #self.img = ImageQt.ImageQt(Image.open(BytesIO(response.content)))
+        #self.label.setPixmap(QPixmap.fromImage(self.img))
 
 
 def except_hook(cls, exception, traceback):
